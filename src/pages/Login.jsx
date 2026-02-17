122c34e2e033f94f2a57929cb4b5911eda6066cf
castial004 <nm6395825@gmail.com>
2026-02-17 09:14:41 +0530

    handled error diplaying in the form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
-import {React,useState} from 'react'
+import { React, useState } from 'react'
 import authService from '../Springboot/Auth'
 import Input from '../components/Input'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { login as loginAction } from '../store/AuthSlice'
 const Login = () => {
@@ -11,30 +11,42 @@ const Login = () => {
     const [error, setError] = useState('')
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    const handleSubmit = async (e) => {
-         e.preventDefault()
-        try {
-            const dbUser = await authService.login({username, password})
-            if (dbUser) {
-                dispatch(loginAction(dbUser))
-                navigate("/")
-            }
-        } catch(error){
-            setError(error)
+const handleSubmit = async (e) => {
+    
+    e.preventDefault()
+    setError("")
+
+    try {
+        const dbUser = await authService.login({ username, password })
+
+        if (dbUser) {
+            dispatch(loginAction(dbUser))
+            navigate("/")
         }
-        
+
+    } catch (error) {
+        setError(error.message)
     }
+}
     return (
-        <div className='flex items-center justify-center'>
-            <form onSubmit={handleSubmit}>
-                <Input name="username" label="UserName: " placeholder="your username" value={username} onChange={(e)=>setUsername(e.target.value)}/>
+        <div className='flex items-center justify-center bg-gray-400 h-screen'>
+            <form onSubmit={handleSubmit} className='bg-gray-200 px-10 py-10 rounded-xl relative'>
+                <div className='mb-5 mx-51'>
+                    <h1 className='text-3xl mb-3'><b>Login to your account</b></h1>
+                    <h2>Don't have an account ? <span className='text-green-400 hover:text-green-600'><Link to="/signup">Signup</Link></span></h2>
+                </div>
+                <div className='flex justify-center items-center text-xl'>
+                    {error && (<p className='text-red-500'>{error}</p>)}
+                </div>
+                <Input name="username" label="UserName: " placeholder="your username" value={username} onChange={(e) => setUsername(e.target.value)} />
                 {/* <Input name="email" label="UserName: " placeholder="your email" value={username}/> */}
-                <Input name="password" label="Passowrd: " placeholder="your password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-                <button className='w-full bg-green-400 hover:bg-green-200 rounded-2xl' type='submit'>Submit</button>
+                <Input name="password" label="Passowrd: " placeholder="your password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <div className='mx-40'>
+                    <button className='bg-green-400 hover:bg-green-300 rounded w-full py-4' type='submit'>Submit</button>
+                </div>
+
             </form>
-            <div className='flex justify-center items-center'>
-                {error && (<p className='text-red-500'>{error}</p>)}
-            </div>
+
         </div>
     )
 }
