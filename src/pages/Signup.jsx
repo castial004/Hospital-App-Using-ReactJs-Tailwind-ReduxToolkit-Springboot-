917e698944b5749fc26413a1d0e317c4e6b8dc71
castial004 <nm6395825@gmail.com>
2026-02-17 09:13:39 +0530

    successfully displayed error and successfull message to user

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,36 +1,52 @@
 import React, { useState } from 'react'
 import Input from '../components/Input'
 import authService from '../Springboot/Auth'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate,Link } from 'react-router-dom'
 const Signup = () => {
     const [username, setUsername] = useState("")
     // const [email,setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [error, setError] = useState('')
+    const [message, setMessage] = useState('')
     const navigate = useNavigate()
     const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-            const dbUser = await authService.createAccount({ username, password })
-            if (dbUser) {
+    e.preventDefault()
+    setError("") 
+
+    try {
+        const dbUser = await authService.createAccount({ username, password })
+
+        if (dbUser) {
+            setMessage("Creaed Account sucessfully: you will we redirected to login page")
+            setTimeout(()=>{
                 navigate("/login")
-            }
-        } catch(error){
-            setError(error)
+            },8000)    
         }
-        
+
+    } catch (error) {
+        setError(error.message || "Please enter valid credentials")
     }
+}
     return (
-        <div className='flex items-center justify-center'>
-            <form onSubmit={handleSubmit}>
+        <div className='flex items-center justify-center bg-gray-400 h-screen'>
+            <form onSubmit={handleSubmit} className='bg-gray-200 px-10 py-10 rounded-xl'>
+                <div className='mb-5 mx-29 text-center'>
+                    <h1 className='text-3xl mb-3'><b>Sign in to create your new account</b></h1>
+                    <h2>Already have an account ? <span className='text-green-400 hover:text-green-600'><Link to="/Login">Login</Link></span></h2>
+                </div>
+                <div className='flex justify-center items-center text-xl'>
+                    {error && (<p className='text-red-500'>{error}</p>)}
+                    {message && (<p className='text-green-500'>{message}</p>)}
+                </div>
                 <Input name="username" label="UserName: " placeholder="your username" value={username} onChange={(e) => setUsername(e.target.value)} />
                 {/* <Input name="email" label="UserName: " placeholder="your email" value={username}/> */}
                 <Input name="password" label="Passowrd: " placeholder="your password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button className='w-full bg-green-400 hover:bg-green-200 rounded-2xl' type='submit'>Submit</button>
+                <div className='mx-40'>
+                    <button className='bg-green-400 hover:bg-green-300 rounded w-full py-4' type='submit'>Submit</button>
+                </div>
+
             </form>
-            <div className='flex justify-center items-center'>
-                {error && (<p className='text-red-500'>{error}</p>)}
-            </div>
+
         </div>
     )
 }
