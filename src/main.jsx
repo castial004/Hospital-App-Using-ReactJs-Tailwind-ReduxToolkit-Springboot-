821ff54b3afbfba1199e859bd11ca3dc47898bf0
castial004 <nm6395825@gmail.com>
2026-02-19 04:34:27 +0530

    defined routes and store in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './Home.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import { Provider } from 'react-redux'
-import Store from "./store/Store.js"
-
+import { RedirectAuthContainer,Home, Signup, Login, Store ,App} from './index'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={Store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<App />} >
+            <Route  index element={
+                <Home />
+            } />
+            <Route path="/login" element={
+              <RedirectAuthContainer  authentication={false}>
+               <Login/>
+              </RedirectAuthContainer>
+            } />
+            <Route path="/signup" element={
+              <RedirectAuthContainer authentication={false}>
+               <Signup/>
+              </RedirectAuthContainer>
+            } />
+          </Route>
         </Routes>
       </BrowserRouter>
     </Provider>
