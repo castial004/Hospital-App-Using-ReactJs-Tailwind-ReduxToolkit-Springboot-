fa86f3a1acdb5318c47f37295d24a6725c991aa1
castial004 <nm6395825@gmail.com>
2026-02-19 04:36:02 +0530

    imported and exported all component for clean code

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import Signup from './pages/Signup.jsx'
 import Login from './pages/Login.jsx'
 import Home from './Home.jsx'
 import Store from "./store/Store.js"
-export{Signup,Login,Home,Store}
+import Footer from "./components/Footer";
+import Logo from './components/Logo.jsx'
+import Header from './components/Header.jsx'
+import RedirectAuthContainer from './components/RedirectAuthContainer.jsx';
+import App from './App.jsx';
+import GoogleLoginBtn from './components/GoogleLoginBtn.jsx';
+export{Signup,Login,Home,Store,Footer,Logo,Header,RedirectAuthContainer,App,GoogleLoginBtn}
