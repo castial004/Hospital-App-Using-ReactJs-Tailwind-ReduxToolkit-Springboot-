3410ae1166da2a73433e9f09149a117c56babc6b
castial004 <nm6395825@gmail.com>
2026-02-20 18:41:42 +0530

    added a logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,12 @@ import { Link, useNavigate } from "react-router-dom";
 import { Container } from "./Container";
 import Logo from "./Logo";
 import {useSelector} from "react-redux";
-export const Header = () => {
+import { useDispatch } from 'react-redux'
+import { logout as logoutAction } from '../store/AuthSlice'
+const Header = () => {
     const navigate = useNavigate();
     const authStatus = useSelector((state)=>state.auth.status)
+    const dispatch = useDispatch()
     const navItems = [
         { name: "Home", path: "/",active:authStatus},
         { name: "Login", path: "/login",active:!authStatus},
@@ -12,14 +15,18 @@ export const Header = () => {
         { name: "Appointment", path: "/appointment",active:authStatus },
         { name: "Insurance", path: "/insurance",active:authStatus}
     ]
+    function handleLogout(){
+        dispatch(logoutAction())
+        navigate("/")
+    }
     return (
-        <header className="bg-gray-700">
+        <header className="bg-gray-700 fixed top-0 w-full z-50">
             <Container>
                     <nav className="flex w-full items-center">
                         <div className="w-10">
-                            <Logo />
+                            <Logo className="rounded-full"/>
                         </div>
-                        <div className="ml-auto">
+                        <div className="ml-auto ">
                             {navItems.map(item =>{
                                     if(item.active==true){
                                         return(
@@ -30,6 +37,7 @@ export const Header = () => {
                                     }
                                 }
                             )}
+                            {authStatus && <button onClick={handleLogout} className={`bg-red-600  rounded-3xl mx-1 my-3 hover:bg-red-300 hover:cursor-pointer px-2 py-1`}>Logout</button>}
                         </div>
                     </nav>
                 
@@ -37,3 +45,4 @@ export const Header = () => {
         </header>
     )
 }
+export default Header
