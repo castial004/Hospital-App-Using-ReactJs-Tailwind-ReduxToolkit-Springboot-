cb706644c60b7fe6750a3af3173c4ff3196c7520
castial004 <nm6395825@gmail.com>
2026-02-27 21:49:56 +0530

    logo component for our app

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import { Navigate, useNavigate } from "react-router-dom"
 
-const Logo = () => {
+const Logo = ({ className = "" }) => {
+  const navigate = useNavigate()
   return (
-      <img src="https://www.bing.com/th/id/OIP.NTLQIjOH20bTTxJFHt-ZsgHaHa?w=218&h=211&c=8&rs=1&qlt=90&r=0&o=6&dpr=1.4&pid=3.1&rm=2"
+    <img
+      onClick={()=>navigate("/")}
+      src="https://www.bing.com/th/id/OIP.NTLQIjOH20bTTxJFHt-ZsgHaHa?w=218&h=211&c=8&rs=1&qlt=90&r=0&o=6&dpr=1.4&pid=3.1&rm=2"
       alt="Logo"
-      className="object-contain w-full h-auto"/>
+      className={`object-contain ${className} hover:cursor-pointer`}
+    />
   )
 }
 
